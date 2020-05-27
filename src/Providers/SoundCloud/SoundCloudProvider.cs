1cbe1e164ef0d1a9b227ee234f6777fcb7611d87
Yucked <yucked@protonmail.com>
2020-05-27 16:41:03 -0400

    Updating SoundCloud API

--- src/Providers/SoundCloud/SoundCloudProvider.cs
+++ src/Providers/SoundCloud/SoundCloudProvider.cs
@@ -14,147 +14,151 @@ namespace Dysc.Providers.SoundCloud {
     /// <summary>
     /// </summary>
     public sealed class SoundCloudProvider : IAudioProvider {
-        private const string BASE_URL = "https://api.soundcloud.com";
-        private readonly SoundCloudParser _cloudParser;
-        private readonly HttpClient _httpClient;
+		private const string BASE_URL = "https://api-v2.soundcloud.com";
+		private readonly SoundCloudParser _cloudParser;
+		private readonly HttpClient _httpClient;
 
         /// <summary>
-        /// 
         /// </summary>
         /// <param name="httpClient"></param>
         public SoundCloudProvider(HttpClient httpClient) {
-            _httpClient = httpClient;
-            _cloudParser = new SoundCloudParser(_httpClient);
-        }
+			_httpClient = httpClient;
+			_cloudParser = new SoundCloudParser(_httpClient);
+		}
 
         /// <summary>
         /// </summary>
         /// <param name="httpClientFactory"></param>
         public SoundCloudProvider(IHttpClientFactory httpClientFactory)
-            : this(httpClientFactory.CreateClient(nameof(SoundCloudProvider))) { }
+			: this(httpClientFactory.CreateClient(nameof(SoundCloudProvider))) {
+		}
 
-        /// <inheritdoc />
-        public async ValueTask<SearchResponse> SearchAsync(string query) {
-            await _cloudParser.ValidateClientIdAsync()
-                .ConfigureAwait(false);
+		/// <inheritdoc />
+		public async ValueTask<SearchResponse> SearchAsync(string query) {
+			await _cloudParser.ValidateClientIdAsync()
+			   .ConfigureAwait(false);
 
-            var searchResponse = new SearchResponse {
-                Query = query
-            };
+			var searchResponse = new SearchResponse {
+				Query = query
+			};
 
-            var url = "";
-            switch (query) {
-                case var q when Uri.IsWellFormedUriString(query, UriKind.Absolute):
-                    if (!q.Contains("sets")) {
-                        url = BASE_URL
-                            .WithPath("resolve")
-                            .WithParameter("url", query)
-                            .WithParameter("client_id", SoundCloudParser.ClientId);
+			var url = "";
+			switch (query) {
+				case var q when Uri.IsWellFormedUriString(query, UriKind.Absolute):
+					if (!q.Contains("sets")) {
+						url = BASE_URL
+						   .WithPath("resolve")
+						   .WithParameter("url", query)
+						   .WithParameter("client_id", SoundCloudParser.ClientId);
 
-                        searchResponse.Status = SearchStatus.TrackLoaded;
-                    }
-                    else {
-                        url = BASE_URL
-                            .WithPath("resolve")
-                            .WithParameter("url", query)
-                            .WithParameter("client_id", SoundCloudParser.ClientId);
+						searchResponse.Status = SearchStatus.TrackLoaded;
+					}
+					else {
+						url = BASE_URL
+						   .WithPath("resolve")
+						   .WithParameter("url", query)
+						   .WithParameter("client_id", SoundCloudParser.ClientId);
 
-                        searchResponse.Status = SearchStatus.PlaylistLoaded;
-                    }
+						searchResponse.Status = SearchStatus.PlaylistLoaded;
+					}
 
-                    break;
+					break;
 
-                case var _ when !Uri.IsWellFormedUriString(query, UriKind.Absolute):
-                    url = BASE_URL
-                        .WithPath("tracks")
-                        .WithParameter("q", query)
-                        .WithParameter("client_id", SoundCloudParser.ClientId);
+				case var _ when !Uri.IsWellFormedUriString(query, UriKind.Absolute):
+					url = BASE_URL
+					   .WithPath("search")
+					   .WithParameter("q", query)
+					   .WithParameter("client_id", SoundCloudParser.ClientId);
 
-                    searchResponse.Status = SearchStatus.SearchResult;
-                    break;
-            }
+					searchResponse.Status = SearchStatus.SearchResult;
+					break;
+			}
 
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
-            using var responseMessage = await _httpClient.SendAsync(requestMessage)
-                .ConfigureAwait(false);
+			using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+			using var responseMessage = await _httpClient.SendAsync(requestMessage)
+			   .ConfigureAwait(false);
 
-            if (!responseMessage.IsSuccessStatusCode)
-                return searchResponse.WithError();
+			if (!responseMessage.IsSuccessStatusCode) {
+				return searchResponse.WithError();
+			}
 
-            var byteArray = await responseMessage.Content
-                .ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+			var byteArray = await responseMessage.Content
+			   .ReadAsByteArrayAsync()
+			   .ConfigureAwait(false);
 
-            if (byteArray.Length == 0)
-                return searchResponse.WithError();
+			if (byteArray.Length == 0) {
+				return searchResponse.WithError();
+			}
 
-            switch (searchResponse.Status) {
-                case SearchStatus.TrackLoaded:
-                    var scTrack = JsonSerializer.Deserialize<SoundCloudTrack>(byteArray);
-                    searchResponse.AddTrack(scTrack.ToTrackInfo);
-                    break;
+			switch (searchResponse.Status) {
+				case SearchStatus.TrackLoaded:
+					var scTrack = JsonSerializer.Deserialize<SoundCloudTrack>(byteArray);
+					searchResponse.AddTrack(scTrack.ToTrackInfo);
+					break;
 
-                case SearchStatus.PlaylistLoaded:
-                    var scPly = JsonSerializer.Deserialize<SoundCloudPlaylist>(byteArray);
-                    searchResponse.Playlist = scPly.ToPlaylistInfo;
-                    searchResponse.AddTracks(scPly.Tracks.Select(x => x.ToTrackInfo));
+				case SearchStatus.PlaylistLoaded:
+					var scPly = JsonSerializer.Deserialize<SoundCloudPlaylist>(byteArray);
+					searchResponse.Playlist = scPly.ToPlaylistInfo;
+					searchResponse.AddTracks(scPly.Tracks.Select(x => x.ToTrackInfo));
 
-                    break;
+					break;
 
-                case SearchStatus.SearchResult:
-                    var scTracks = JsonSerializer.Deserialize<IEnumerable<SoundCloudTrack>>(byteArray);
-                    searchResponse.AddTracks(scTracks.Select(x => x.ToTrackInfo));
-                    break;
-            }
+				case SearchStatus.SearchResult:
+					var scTracks = JsonSerializer.Deserialize<IEnumerable<SoundCloudTrack>>(byteArray);
+					searchResponse.AddTracks(scTracks.Select(x => x.ToTrackInfo));
+					break;
+			}
 
-            return searchResponse.Tracks.Count != 0
-                ? searchResponse
-                : searchResponse.WithNoMatches();
-        }
+			return searchResponse.Tracks.Count != 0
+				? searchResponse
+				: searchResponse.WithNoMatches();
+		}
 
-        /// <inheritdoc />
-        public async ValueTask<Stream> GetStreamAsync(string trackUrl) {
-            if (!Uri.IsWellFormedUriString(trackUrl, UriKind.Absolute))
-                throw new UriFormatException($"{nameof(trackUrl)} must be an absolute uri string.");
+		/// <inheritdoc />
+		public async ValueTask<Stream> GetStreamAsync(string trackUrl) {
+			if (!Uri.IsWellFormedUriString(trackUrl, UriKind.Absolute)) {
+				throw new UriFormatException($"{nameof(trackUrl)} must be an absolute uri string.");
+			}
 
-            var requestMessage = new HttpRequestMessage(HttpMethod.Get, trackUrl);
-            var responseMessage = await _httpClient.SendAsync(requestMessage)
-                .ConfigureAwait(false);
+			var requestMessage = new HttpRequestMessage(HttpMethod.Get, trackUrl);
+			var responseMessage = await _httpClient.SendAsync(requestMessage)
+			   .ConfigureAwait(false);
 
-            var byteArray = await responseMessage.Content.ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+			var byteArray = await responseMessage.Content.ReadAsByteArrayAsync()
+			   .ConfigureAwait(false);
 
-            var transcoding = SoundCloudParser.FetchBestTranscoding(byteArray);
-            requestMessage = new HttpRequestMessage(HttpMethod.Get, transcoding.Url);
-            responseMessage = await _httpClient.SendAsync(requestMessage)
-                .ConfigureAwait(false);
+			var transcoding = SoundCloudParser.FetchBestTranscoding(byteArray);
+			requestMessage = new HttpRequestMessage(HttpMethod.Get, transcoding.Url);
+			responseMessage = await _httpClient.SendAsync(requestMessage)
+			   .ConfigureAwait(false);
 
-            byteArray = await responseMessage.Content.ReadAsByteArrayAsync()
-                .ConfigureAwait(false);
+			byteArray = await responseMessage.Content.ReadAsByteArrayAsync()
+			   .ConfigureAwait(false);
 
-            if (!JsonDocument.Parse(byteArray).RootElement.TryGetProperty("url", out var url))
-                throw new Exception("SoundCloud API did not return track's stream url.");
+			if (!JsonDocument.Parse(byteArray).RootElement.TryGetProperty("url", out var url)) {
+				throw new Exception("SoundCloud API did not return track's stream url.");
+			}
 
-            requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{url}");
-            responseMessage = await _httpClient.SendAsync(requestMessage)
-                .ConfigureAwait(false);
+			requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{url}");
+			responseMessage = await _httpClient.SendAsync(requestMessage)
+			   .ConfigureAwait(false);
 
-            var stream = await responseMessage.Content.ReadAsStreamAsync()
-                .ConfigureAwait(false);
-            stream.Position = 0;
+			var stream = await responseMessage.Content.ReadAsStreamAsync()
+			   .ConfigureAwait(false);
+			stream.Position = 0;
 
-            return stream;
-        }
+			return stream;
+		}
 
-        /// <inheritdoc />
-        public ValueTask<Stream> GetStreamAsync(TrackInfo track) {
-            return GetStreamAsync(track.Url);
-        }
+		/// <inheritdoc />
+		public ValueTask<Stream> GetStreamAsync(TrackInfo track) {
+			return GetStreamAsync(track.Url);
+		}
 
-        /// <inheritdoc />
-        public ValueTask DisposeAsync() {
-            _httpClient.Dispose();
-            return default;
-        }
-    }
+		/// <inheritdoc />
+		public ValueTask DisposeAsync() {
+			_httpClient.Dispose();
+			return default;
+		}
+	}
 }
